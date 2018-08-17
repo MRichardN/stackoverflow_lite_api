2bb9f71c3dc3df17cd725f0cf4a3e1c648503d88
MRichardN <mathengerichard29@gmail.com>
2018-08-17 10:42:59 +0300

    add multiple questions requests

--- app/__init__.py
+++ app/__init__.py
@@ -1,35 +1,20 @@
 # app/__init__.py
 
-from flask import Flask
+#from flask import Flask
 
 # local import
 from instance.config import app_config
 
 # Load the views
 
-from app import views
+#from app import views
 
 def create_app(config_name):
-    from app import views
-    app = Flask(__name__, instance_relative_config=True)
+    #from app import views
+    #app = Flask(__name__, instance_relative_config=True)
+    from .views import app
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     return app
 
 
-
-
-
-#added
-# Initialize the app
-#app = Flask(__name__, instance_relative_config=True)
-
-
-
-# Load the config file
-#instance.config.from_object('config')
-
-
-
-#removed from def
-#app = FlaskAPI(__name__, instance_relative_config=True)
--- app/views.py
+++ app/views.py
@@ -1,18 +1,15 @@
 #app/views.py
 
 from app.__init__ import create_app
-#import json
+
 from flask import Flask
 from flask import request, jsonify, abort, make_response
 
-#from instance.config import app_config
+
+app = Flask(__name__, instance_relative_config=True)
 
 
-#@app.errorhandler(404)
-#def not_found(error):
- #   return make_response(jsonify({'error': 'Not found'}), 404)
-
-app = Flask(__name__)
+            
 
 questions = [
     {
@@ -32,14 +29,14 @@ questions = [
 
 
 #GET all questions
-#@app.route('/app/api/v1/questions', methods=['GET'])
-#def get_questions():
-#    return jsonify({'questions': questions})
+@app.route('/app/api/v1/questions', methods=['GET'])
+def get_questions():
+    return jsonify({'questions': questions})
 
   
 
-#GET a single question app/api/v1/questions/<int:question_id>
-@app.route('/stack/api/v1/questions', methods=['GET'])
+#GET a single question 
+@app.route('/app/api/v1/questions/<int:question_id>', methods=['GET'])
 def get_question(question_id):
     question = [question for question in questions if question['id'] == question_id]
     if len(question) == 0:
@@ -52,7 +49,7 @@ def create_question():
     if not request.json or not 'language' in request.json:
         abort(400)
     question = {
-        'id': tasks[-1]['id'] + 1,
+        'id': questions[-1]['id'] + 1,
         'language': request.json['language'],
         'date_posted': request.json.get('date_posted', "")
     }
@@ -67,16 +64,21 @@ def update_question(question_id):
         abort(404)
     if not request.json:
         abort(400)
+    #if 'id' in request.json and type(request.json['id']) != unicode:
+       # abort(400)    
     if 'language' in request.json and type(request.json['language']) != unicode:
         abort(400)
     if 'ask' in request.json and type(request.json['ask']) is not unicode:
         abort(400)
     if 'date_posted' in request.json and type(request.json['date_posted']) is not unicode:
         abort(400)
-    task[0]['language'] = request.json.get('language', task[0]['language'])
-    task[0]['ask'] = request.json.get('ask', task[0]['ask'])
-    task[0]['date_posted'] = request.json.get('date_posted', task[0]['date_posted'])
+    question[0]['id'] = request.json.get('id', question[0]['id'])    
+    question[0]['language'] = request.json.get('language', question[0]['language'])
+    question[0]['ask'] = request.json.get('ask', question[0]['ask'])
+    question[0]['date_posted'] = request.json.get('date_posted', question[0]['date_posted'])
     return jsonify({'question': question[0]})
+
+    
 
 @app.route('/app/api/v1/questions/<int:question_id>', methods=['DELETE'])
 def delete_question(task_id):
@@ -84,7 +86,34 @@ def delete_question(task_id):
     if len(question) == 0:
         abort(404)
     questions.remove(question[0])
-    return jsonify({'result': True})     
+    return jsonify({'result': True})   
+
+
+
+users =[]
+
+@app.route('/app/api/v1/sign_up', methods=['POST'])
+def sign_up():
+    user = {'id': len(users)+1,
+        'user_name': request.json.get('user_name'),
+        'email': request.json.get('email'),
+        'password': request.json.get('password')
+    }
+    users.append(user)
+    return jsonify({'message': 'Registration successful', 'User': users}), 201
+
+@app.route('/app/api/v1/login', methods=['POST'])
+def signin():
+    user_name= request.json.get("user_name")
+    password= request.json.get("password")
+
+    if user_name == "" or password == "":
+        return "Please enter your user_name and password to log in"
+    user = [user for user in users if user['user_name'] == user_name and user['password'] == password]
+
+    if not user:
+        return jsonify({'message': 'New user?, Please sign_up'})
+    return jsonify({'message': 'Logged in successsfully'})      
 
 if __name__ == '__main__':
     app.run(debug=True)
