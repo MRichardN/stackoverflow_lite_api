c803dcbd2f13b339b431cc9e9561dfca3839d8c0
MRichardN <mathengerichard29@gmail.com>
2018-08-18 22:15:20 +0300

    [bug] fix def_posting_a_question

--- tests/test_questions.py
+++ tests/test_questions.py
@@ -5,39 +5,40 @@ import os
 
 from app.views import create_app
 
-class StackOverflow_endpoint(unittest.TestCase):
+class TestStackOverflow_endpoint(unittest.TestCase):
+    """This class represents stackoverflow-lite endpoint test case"""
     
     def setUp(self):
         """Define the test variables and initialize the application."""
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client()
-        self.questions = {'questions':'What is the difference between django and flask','python' 'date_posted': '7th May 2017'}
+        self.questions = {'id':1,'ask':'What is the difference between django and flask','language':'python', 'date_posted': '7th May 2017'}
         
     def test_posting_of_questions(self):
-        '''test user can post a question'''
+        """test user can post a question (POST request)"""
         response = self.client.post(
             '/api/v1/questions', data=json.dumps(self.questions), content_type='application/json')
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 201)
 
     def test_viewing_questions(self):
         """Test user can view all questions."""
         response = self.client.get(
-            '/app/api/v1/questions', content_type='application/json')
+            '/api/v1/questions', content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
     def test_viewing_a_single_question(self):
         """Test user can view a single question."""
         response = self.client.get(
-            '/app/api/v1/questions/1', data=json.dumps(self.questions), content_type='application/json')
+            '/api/v1/questions/1', data=json.dumps(self.questions), content_type='application/json')
         self.assertEqual(response.status_code, 200)
     
     def test_editing_a_question(self):
-        """Test use rcan edit a question."""
+        """Test use can edit a question."""
         self.client.post(
             '/api/v1/questions', data=json.dumps(self.questions), content_type='application/json')
         response = self.client.put(
             '/api/v1/questions/1', data=json.dumps(self.questions), content_type='application/json')
-        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 200)
         #200
 
 if __name__ == "__main__":
