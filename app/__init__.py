e64f14dcada9eccf855dff59f3769062f195a665
MRichardN <mathengerichard29@gmail.com>
2018-08-18 08:38:55 +0300

    added development in run.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,7 @@
-# app/__init__.py
-
-#from flask import Flask
-
-# local import
+#app/__init__.py
 from instance.config import app_config
 
-# Load the views
-
-#from app import views
-
 def create_app(config_name):
-    #from app import views
-    #app = Flask(__name__, instance_relative_config=True)
     from .views import app
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
--- run.py
+++ run.py
@@ -6,7 +6,7 @@ import os
 from app.__init__ import create_app
 
 config_name = os.getenv('APP_SETTINGS') # config_name = "development"
-app = create_app(config_name)
+app = create_app("development")
 
 @app.route('/')
 def index():
