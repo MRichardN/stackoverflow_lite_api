bd7edbc3af43905cb7328995b85e78ae7d5f9464
MRichardN <mathengerichard29@gmail.com>
2018-08-18 08:41:22 +0300

    modified run file

--- run.py
+++ run.py
@@ -8,9 +8,6 @@ from app.__init__ import create_app
 config_name = os.getenv('APP_SETTINGS') # config_name = "development"
 app = create_app("development")
 
-@app.route('/')
-def index():
-    return "Hello world"
 
  
 
