05d77ce8f40b589dff5fe21c5423e14a968764db
MRichardN <mathengerichard29@gmail.com>
2018-08-21 23:19:29 +0300

    [feature] add test answers end point

---
+++ tests/test_answers.py
@@ -0,0 +1,31 @@
+import unittest
+import json
+import os
+
+
+from app.views import create_app
+
+class TestStackOverflow_endpoint_answers(unittest.TestCase):
+    """This class represents stackoverflow-lite endpoint test case"""
+    
+    def setUp(self):
+        """Define the test variables and initialize the application."""
+        self.app = create_app(config_name="testing")
+        self.client = self.app.test_client()
+        self.answers = {'id':1,'ask':'What is the difference between django and flask','language':'python', 'date_posted': '7th May 2017'}
+    
+    def test_viewing_answers(self):
+        """Test user can view all answers."""
+        response = self.client.get(
+            '/api/v1/questions/answers', content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
+    def test_viewing_a_single_answer(self):
+        """Test user can view a single answer."""
+        response = self.client.get(
+            '/api/v1/questions/1', data=json.dumps(self.answers), content_type='application/json')
+        self.assertEqual(response.status_code, 200)    
+
+
+if __name__ == "__main__":
+    unittest.main()        
