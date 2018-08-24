bbb9ed3c6e52ded41153f739e2799df5a943b5ad
MRichardN <mathengerichard29@gmail.com>
2018-08-24 08:52:46 +0300

    [fix] modify user test

--- tests/test_users.py
+++ tests/test_users.py
@@ -3,7 +3,7 @@ import os
 import json
 
 from app.views import create_app
-#from app.models import User
+from app.models import User
 
 class StackOverflow_endpoint_Users(unittest.TestCase):
     """This class represent application users."""
@@ -12,18 +12,18 @@ class StackOverflow_endpoint_Users(unittest.TestCase):
         """Define test variables and initialize."""
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client()
-        self.users = {'user_name': 'James Bond', 'email': 'jamesbond@gmail.com', 'password': 'james'}
+        self.users = {'name': 'James Bond', 'email': 'jamesbond@gmail.com', 'password': 'james'}
 
     def test_registering_new_user(self):
         """Test user sign_up."""
         response = self.client.post(
-            '/api/v1/sign_up', data=json.dumps(self.users), content_type='application/json')
+            '/api/v1/auth/signup', data=json.dumps(self.users), content_type='application/json')
         self.assertEqual(response.status_code, 201)
 
     def test_user_login(self):
         """Test user login."""
         response = self.client.post(
-            '/api/v1/login', data=json.dumps(self.users), content_type='application/json')
+            '/api/v1/auth/login', data=json.dumps(self.users), content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
 if __name__ == "__main__":
